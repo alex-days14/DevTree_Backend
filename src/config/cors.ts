b0659e5b99d43723bfd4eae7b3fdeefa8e379016
alex-days14 <a01240177@tec.mx>
2025-01-17 14:48:42 -0700

    origin

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -4,10 +4,11 @@ export const corsConfig: CorsOptions = {
     origin: function (origin, callback) {
         const WHITELIST = process.env.FRONTEND_URL.split(";")
 
-        if(process.argv.includes("--api")){
+        if (process.argv.includes("--api")) {
             WHITELIST.push(undefined)
         }
-
+        console.log(WHITELIST)
+        console.log(origin)
         if (WHITELIST.includes(origin)) {
             callback(null, true)
         } else {
